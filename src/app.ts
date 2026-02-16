9e2fefdaf81e2b34234b52799c431a28b07789ce
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2026-02-17 05:09:28 +0530

    chore: fixed origin

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+        "https://yummmzo.com",
+        "https://www.yummmzo.com",
+        process.env.FRONTEND_URL || "http://localhost:5173"
+    ],
     credentials: true
 }));
 app.use(express.json());
